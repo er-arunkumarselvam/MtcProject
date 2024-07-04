a452404839bde542a258949c232eb039c0f9cdd4
er-arunkumarselvam <er.arunkumar_selvam@outlook.com>
2024-07-04 16:47:21 +0530

    React Installation and login page implementation

---
+++ web-ui/src/components/pages/Scanner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ScannerElement from '../elements/ScannerElement'
+
+export default function Scanner() {
+  return (
+    <>
+    <h1>Scanner</h1>
+        <ScannerElement/>
+    </>
+  )
+}
