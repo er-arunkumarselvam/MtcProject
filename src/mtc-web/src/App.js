ab0deacd31c56a4891d0727cdbadd5b95bf95146
er-arunkumarselvam <er.arunkumar_selvam@outlook.com>
2024-07-13 20:42:46 +0530

    Navbar Inclusion

--- src/mtc-web/src/App.js
+++ src/mtc-web/src/App.js
@@ -1,28 +1,38 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Login from "./pages/Login";
 import Scanner from "./pages/Scanner";
 import Form from "./pages/Form";
 import { Provider } from "react-redux";
-import store from "./store/store"
+import store from "./store/store";
+import ProtectedRoute from "./router/ProtectedRoute";
+import { UserNavbar } from "./components/UserNavbar";
 
-function App() {
+const App = () => {
   return (
-    <>
     <Provider store={store}>
       <BrowserRouter>
-      <Routes>
-        <Route path="/" Component={Login}/>
-        <Route path="/scanner" Component={Scanner}/>
-        <Route path="/form" Component={Form}/>
-      </Routes>
-    </BrowserRouter>
-
+        <AppRoutes />
+      </BrowserRouter>
     </Provider>
-    
-  
-    
+  );
+};
+
+const AppRoutes = () => {
+  const location = useLocation();
+  const showNavbar = location.pathname !== "/";
+
+  return (
+    <>
+      {showNavbar && <UserNavbar />}
+      <Routes>
+        <Route path="/" Component={Login} />
+        <Route Component={ProtectedRoute}>
+          <Route path="/scanner" Component={Scanner} />
+          <Route path="/form" Component={Form} />
+        </Route>
+      </Routes>
     </>
   );
-}
+};
 
 export default App;
