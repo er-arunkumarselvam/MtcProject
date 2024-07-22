d17df61e4711f0ba73e86be151d591cc97318713
ThiruvengadenR <thiruvengadenr2002@gmail.com>
2024-07-22 22:44:15 +0530

    update cross origin

--- src/main/java/com/example/demo/Security/SecurityConfiguration.java
+++ src/main/java/com/example/demo/Security/SecurityConfiguration.java
@@ -60,7 +60,7 @@ public class SecurityConfiguration {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         //configuration.setAllowedOrigins(List.of("http://localhost:3000")); // Frontend URL
-        configuration.setAllowedOrigins(List.of("https://mtcinspection.onrender.com")); 
+        configuration.setAllowedOrigins(List.of("https://mtcreact.onrender.com")); 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
